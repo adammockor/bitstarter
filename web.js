05819cbd86390d24808ff49ac842bd86d02b92dd
Adam Mockor <adam.mockor@gmail.com>
2013-07-10 14:42:49 +0200

    Bla bla

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var buf = new buffer(fs.readFileSync('index.html'));
+	console.log(buf.toString());  
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
